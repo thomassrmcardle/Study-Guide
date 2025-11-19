07f9446323e8a42533cd53f9ba706a5d3c7fc954
Thomas <thomas_mcardle@outlook.com>
2025-11-19 20:32:59 +0000

    Replaced Link tag with a in js.

--- guide-app/src/components/navbar.js
+++ guide-app/src/components/navbar.js
@@ -1,17 +1,15 @@
-"use client";
-
 import Link from 'next/link';
 import React from 'react';
 
 export default function Navbar() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
-      <Link className='link' href="/">Home</Link> |{' '}
-      <Link className='link' href="/subjects">Subjects</Link> |{' '}
-      <Link className='link' href="/about">About Us</Link>
+      <a href="/">Home</a> |{' '}
+      <a href="/subjects">Subjects</a> |{' '}
+      <a chref="/about">About Us</a>
       <div className='spacer'></div>
-      <Link className='button' href="/signup">Sign Up</Link>
-      <Link className='link' href="/login">Login</Link>
+      <a className='button' href="/signup">Sign Up</a>
+      <a href="/login">Login</a>
     </nav>
   );
 }
