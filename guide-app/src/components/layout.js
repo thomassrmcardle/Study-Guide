4d905826a923ffdbc5f9513a763d04ad456ad30d
Thomas <thomas_mcardle@outlook.com>
2025-11-19 20:36:45 +0000

    Attempted to fix issue with navbar.

--- guide-app/src/components/layout.js
+++ guide-app/src/components/layout.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Navbar from './navbar';
 
 export default function Layout({ children }) {
   return (
     <div>
-      <Navbar />
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
+        <a href="/">Home</a> |{' '}
+        <a href="/subjects">Subjects</a> |{' '}
+        <a chref="/about">About Us</a>
+        <div className='spacer'></div>
+        <a className='button' href="/signup">Sign Up</a>
+        <a href="/login">Login</a>
+      </nav>
       <main>{children}</main>
     </div>
   );
