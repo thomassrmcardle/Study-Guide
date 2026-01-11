a8640c8d2ac60be6f7b54135515403f0665fcc8e
Thomas <thomas_mcardle@outlook.com>
2026-01-11 17:50:44 +0000

    Updated home page

--- guide-app/src/pages/index.js
+++ guide-app/src/pages/index.js
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <>
       <head>
-        <title>Welcome to Guide</title>
+        <title>Omni | Home</title>
       </head>
       <div style={{ padding: '2rem' }}>
-        <h1><b>The free and effective tool for your studies</b></h1>
+        <h1><b>Today's headlines...</b></h1>
         <div className="row">
           <a className='button' href="/signup">Get Started</a>
           <a className='button secondaryButton'>I have an account</a>
