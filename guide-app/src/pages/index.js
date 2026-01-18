164d66951c018af3c42be2a57f0297934c6208de
Thomas <thomas_mcardle@outlook.com>
2026-01-18 00:17:46 +0000

    Improved sign-up prompt.

--- guide-app/src/pages/index.js
+++ guide-app/src/pages/index.js
@@ -6,9 +6,15 @@ export default function Home() {
       </head>
       <div style={{ padding: '2rem' }}>
         <h1><b>Today's headlines...</b></h1>
-        <div className="row">
-          <a className='button' href="/signup">Get Started</a>
-          <a className='button secondaryButton'>I have an account</a>
+        <div>
+          <div>
+            <h3>Stay connected on what matters to you</h3>
+            <h4>Follow topics and events as they occur</h4>
+          </div>
+          <div className="row">
+            <a className='button' href="/signup">Get Started</a>
+            <a className='button secondaryButton'>I have an account</a>
+          </div>
         </div>
       </div>
     </>
